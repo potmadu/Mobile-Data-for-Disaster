d6e1cdeee1bbb7de72b286de6722d17780aef118
potmadu <potmadu@gmail.com>
2018-02-21 06:34:27 +0700

    update script

--- Mobile Data for Disaster/Mobile Data for Disaster/script.R
+++ Mobile Data for Disaster/Mobile Data for Disaster/script.R
@@ -1,2 +1,17 @@
 datasets_dir = "E:/Datasets/Mobile-Data-for-Disaster/";
 
+lists = list.files(datasets_dir);
+
+for (i in 1:length(lists)) {
+
+    loc_file = paste(datasets_dir,lists[i],sep = "");
+
+    if (i==1) {
+        output = read.csv(loc_file,stringsAsFactors=FALSE);
+    } else {
+        dat = read.csv(loc_file, stringsAsFactors = FALSE);
+        output = rbind(output,dat);
+    }
+
+}
+
