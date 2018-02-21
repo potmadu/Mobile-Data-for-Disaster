2b9f8193437c394385115874f83369861b2a6939
potmadu <potmadu@gmail.com>
2018-02-21 07:12:38 +0700

    update script code

--- Mobile Data for Disaster/Mobile Data for Disaster/script.R
+++ Mobile Data for Disaster/Mobile Data for Disaster/script.R
@@ -1,5 +1,7 @@
 datasets_dir = "E:/Datasets/Mobile-Data-for-Disaster/";
 
+datasets_dir = "C:/Datasets/Mobile-Data-for-Disaster/";
+
 lists = list.files(datasets_dir);
 
 for (i in 1:length(lists)) {
@@ -7,11 +9,27 @@ for (i in 1:length(lists)) {
     loc_file = paste(datasets_dir,lists[i],sep = "");
 
     if (i==1) {
-        output = read.csv(loc_file,stringsAsFactors=FALSE);
+        output = read.csv(loc_file, stringsAsFactors = FALSE);
+        output$week_type = lists[i];
     } else {
         dat = read.csv(loc_file, stringsAsFactors = FALSE);
+        dat$week_type = lists[i];
         output = rbind(output,dat);
     }
 
 }
 
+dat = output;
+
+#############################
+# PREPROCESSING
+#############################
+
+dat_isna = dat[is.na(dat$reporting__number_of_records),];
+dat_filt = dat[!is.na(dat$reporting__number_of_records),];
+
+write.csv(dat_filt,"week-43-52-result.csv");
+
+#############################
+# METHOD
+#############################
