32c4e281c2cf038e676dee289e5647d3634c8b2f
potmadu <potmadu@gmail.com>
2018-02-27 00:39:45 +0700

    Update scripts

--- Mobile Data for Disaster/Mobile Data for Disaster/script.R
+++ Mobile Data for Disaster/Mobile Data for Disaster/script.R
@@ -1,7 +1,5 @@
 datasets_dir = "E:/Datasets/Mobile-Data-for-Disaster/";
 
-datasets_dir = "C:/Datasets/Mobile-Data-for-Disaster/";
-
 lists = list.files(datasets_dir);
 
 for (i in 1:length(lists)) {
@@ -9,16 +7,16 @@ for (i in 1:length(lists)) {
     loc_file = paste(datasets_dir,lists[i],sep = "");
 
     if (i==1) {
-        output = read.csv(loc_file, stringsAsFactors = FALSE);
-        output$week_type = lists[i];
+        output = read.csv(loc_file,stringsAsFactors=FALSE);
     } else {
         dat = read.csv(loc_file, stringsAsFactors = FALSE);
-        dat$week_type = lists[i];
         output = rbind(output,dat);
     }
 
 }
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 dat = output;
 
 #############################
@@ -28,8 +26,20 @@ dat = output;
 dat_isna = dat[is.na(dat$reporting__number_of_records),];
 dat_filt = dat[!is.na(dat$reporting__number_of_records),];
 
+home_antenna_loc = unlist(dat_filt$user_home_antenna__location);
+home_antenna_loc = gsub('\\(','',home_antenna_loc);
+home_antenna_loc = gsub("\\)", "", home_antenna_loc);
+home_antenna_loc = gsub(" ", "", home_antenna_loc);
+home_antenna_loc = strsplit(home_antenna_loc,",");
+
 write.csv(dat_filt,"week-43-52-result.csv");
 
 #############################
 # METHOD
 #############################
+
+
+=======
+>>>>>>> parent of 2b9f819... update script code
+=======
+>>>>>>> parent of 2b9f819... update script code
